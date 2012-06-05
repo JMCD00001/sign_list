dc796917e8476595cba2289c2ba9aa6bc68f5cd3
Jim999 <jim.jmcd@gmail.com>
2012-06-05 10:47:38 -0400

    adam commit for ultimate fun time

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SignList::Application.config.secret_token = 'c3bbb504ac79ca2fa068b8e25ca476073b3b0a493f95bd8293cdf627ac7ffe0878c53df85158eae4113f82721163d0019af1e9e90b7d66df3c397bb63c6f32cb'
+SignList::Application.config.secret_token = '22e3e56177451baf9a152fcd74e2742dd7f0ddfa53838e98f61d39918dd2a55e54c2168fcdc50ef6b4c31fcdbbdfa9e3e448de4ae8a0f06913915078dbce3e35'
